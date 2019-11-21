b67f3cb0a72f0899c3adb438a74b02ebc2dc78e3
Håkon Tansem <hakontan@gmail.com>
2019-11-21 19:18:05 +0100

    Added forward euler method

--- src/task_b.cpp
+++ src/task_b.cpp
@@ -22,6 +22,7 @@ struct CelestialBody {
     arma::vec pos;
     arma::vec vel;
     arma::vec F;
+
     double mass;
     string name;
 
@@ -53,11 +54,16 @@ struct CelestialBody {
 };
 
 class SolarSystem {
-
+    private:
+    arma::vec R;
+    arma::vec F;
 
     public:
     std::vector<CelestialBody> bodies;
 
+    SolarSystem() {
+
+    }
     SolarSystem(std::vector<CelestialBody> bodies_) {
         bodies = bodies_;
     }
@@ -71,7 +77,6 @@ class SolarSystem {
         double vz;
 
 
-
         double m;
         string name;
 
@@ -82,31 +87,120 @@ class SolarSystem {
             bodies.push_back(CelestialBody(x, y, z, vx, vy, vz, m, name));
         }
         infile.close();
-
-        get_force();
+        
+        update_force();
+        
         
     }
-    void get_force() {
-        arma::vec R;
-        arma::vec F;
+    void update_force() {
+            
         for(int i=0; i < bodies.size(); i++) {
             F = arma::zeros(3);
             for(int j=0; j < bodies.size(); j++) {
                 if(i!=j) {
                   R = bodies[i].pos - bodies[j].pos;
                   F += -(G * R * bodies[i].mass * bodies[j].mass)
-                  / std::pow(arma::norm(R), 3);
+                  / std::pow(std::sqrt(R(0) * R(0) + R(1) * R(1) + R(2) * R(2)), 3);
+                       // / std::pow(arma::norm(R, 2), 3);
                 }
             }    
         bodies[i].set_force(F);
-        cout << i << endl;
-        F.print();
+        // F.print();
         }
     }
 };
 
 class Nbody{
+    public:
+    arma::mat x_coords;
+    arma::mat y_coords;
+    arma::mat z_coords;
 
+    arma::mat vx_coords;
+    arma::mat vy_coords;
+    arma::mat vz_coords;
+
+    arma::vec a_i = arma::zeros(3);
+    std::vector<CelestialBody> bodies;
+    double N_bodies;
+    int N;
+    double dt;
+    int writenr;
+    int datapoints;
+    SolarSystem system = SolarSystem();
+
+    Nbody(int steps, double timesteps, int writenr, string filename) {
+
+        N = steps;
+        dt = timesteps;
+        system = SolarSystem(filename);
+        bodies = system.bodies;
+        datapoints = (int) std::round(N/writenr);
+        N_bodies = bodies.size();
+
+        x_coords = arma::zeros(datapoints, N_bodies);
+        y_coords = arma::zeros(datapoints, N_bodies);
+        z_coords = arma::zeros(datapoints, N_bodies);
+
+        vx_coords = arma::zeros(datapoints, N_bodies);
+        vy_coords = arma::zeros(datapoints, N_bodies);
+        vz_coords = arma::zeros(datapoints, N_bodies);
+    }
+
+    void forward_euler() {
+        //double v_prev;
+
+        int c = 0;
+        cout << "no" << endl;
+        bodies[0].pos[0] = 0;
+        bodies[0].pos[1] = 0;
+        bodies[0].pos[2] = 0;
+        bodies[1].pos[0] = 1;
+        bodies[1].pos[1] = 0;
+        bodies[1].pos[2] = 0;
+
+        bodies[1].vel[1] = 6.28;
+        bodies[1].vel[0] = 0;
+        bodies[1].vel[2] = 0;
+        cout << "g" << endl;
+        bodies[1].pos.print();
+
+        for (int i=0; i < N; i++) {
+            //cout << "updating step: " << i << endl;
+            for (int j=0; j<N_bodies; j++) {
+                //cout << bodies[j].name << endl;
+                bodies[j].pos += dt * bodies[j].vel;
+                bodies[j].vel += dt * bodies[j].F / bodies[j].mass;
+                system.update_force();
+                //cout << bodies[j].mass << endl;
+                
+
+                x_coords(i, j) = bodies[j].pos[0];
+                y_coords(i, j) = bodies[j].pos[1];
+                z_coords(i, j) = bodies[j].pos[2];
+
+                vx_coords(i, j) = bodies[j].vel[0];
+                vy_coords(i, j) = bodies[j].vel[1];
+                vz_coords(i, j) = bodies[j].vel[2];
+
+                //vx_coords.save("x_coords.bin", arma_binary);
+                //vy_coords.save("x_coords.bin", arma_binary);
+                //vz_coords.save("x_coords.bin", arma_binary);
+
+            }
+
+            /*
+            if (i == c * datapoints) {
+                 write to matrix
+                
+                c += 1;
+            }
+            */
+        }
+        x_coords.save("x_coords.txt", arma::arma_ascii);
+        y_coords.save("y_coords.txt", arma::arma_ascii);
+        z_coords.save("z_coords.txt", arma::arma_ascii);
+    }
 };
 
 int main(){
@@ -115,16 +209,23 @@ int main(){
     double mass = 0;
     string nam = "jeff";
     CelestialBody jupiter = CelestialBody(0, 0, 0, 0, 0, 0, 0, nam);
-    cout << jupiter.mass << endl;
-    cout << jupiter.name << endl;
-    jupiter.pos.print();
+    //cout << jupiter.mass << endl;
+    //cout << jupiter.name << endl;
+    //jupiter.pos.print();
 
     std::vector<CelestialBody> planets = {jupiter};
-
+    /*
     SolarSystem test_system = SolarSystem(planets);
     cout << test_system.bodies[0].name << endl;
-
+    cout << "1" << endl;
     SolarSystem jeff = SolarSystem("datafiles/planets_data.txt");
-    cout << "jeff" << jeff.bodies[1].F << endl;
+    cout << "2" << endl;
+    cout << "jeff" << jeff.bodies[0].F << endl;
+    cout << "3" << endl;
+    */
+
+    Nbody test = Nbody(100, 0.01, 1,"datafiles/planets_data.txt");
+    test.forward_euler();
+
     return 0;
 }
