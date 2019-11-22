49460949a3e5250fd5dee3ec244c2e6acab54fa8
bernharl <bernharl@student.matnat.uio.no>
2019-11-22 16:39:45 +0100

    Added relativistic force

--- src/SolarSystem.cpp
+++ src/SolarSystem.cpp
@@ -40,12 +40,37 @@ void SolarSystem::update_force_potential() {
             if(i!=j) {
                 R = bodies[i].pos - bodies[j].pos;
                 R_norm = arma::norm(R);
-                F += -(G * R * bodies[i].mass * bodies[j].mass)
-                        / std::pow(R_norm, 3);
+                if (einstein) {
+                    F += calculate_force_einstein(bodies[i].mass,
+                                                  bodies[j].mass,
+                                                  bodies[i].angular_moment(),
+                                                  );
+                }
+                else {
+                    F += calculate_force_newton(bodies[i].mass,
+                                         bodies[j].mass,
+                                         beta,
+                                         );
+                }
                 V += -(G * bodies[j].mass) / R_norm;
             }
         }
     bodies[i].F = F;
     bodies[i].V = V;
+    }
+
+    arma::vec SolarSystem::calculate_force_newton(double mass_i,
+                                                  double mass_j,
+                                                  double beta,
+                                                  ) {
+        return -(G * R * mass_i * mass_j) / std::pow(R_norm, beta + 1);
+    }
+
+    arma::vec SolarSystem::calculate_force_einstein(double mass_i,
+                                                    double mass_j,
+                                                    double l,
+                                                    ) {
+        return -(G * R * mass_i * mass_j) / std::pow(R_norm, 3)
+               * (1 + (3 * l * l / (R * R * C * C) ) );
     }
 }
--- src/SolarSystem.h
+++ src/SolarSystem.h
@@ -12,11 +12,31 @@ class SolarSystem {
 
     public:
         std::vector<CelestialBody> bodies;
+        double beta;
+        bool einstein;
 
         SolarSystem();
         SolarSystem(std::vector<CelestialBody> bodies_);
         SolarSystem(string filename);
         void update_force_potential();
+        arma::vec calculate_force_newton(double mass_i,
+                                         double mass_j,
+                                         double beta,
+                                        )
+        arma::vec calculate_force_newton(double mass_i,
+                                         double mass_j,
+                                        ) {
+                                          calculate_force_newton(mass_i,
+                                                                 mass_j,
+                                                                 2,
+                                                                 );
+                                          }
+
+        arma::vec calculate_force_einstein(double mass_i,
+                                           double mass_j,
+                                           double l,
+                                           )
+
 };
 
 #endif
