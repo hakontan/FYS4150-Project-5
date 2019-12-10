9cc3d1610946084ecc9e1ff5ddf117b2e14db453
Håkon Tansem <hakontan@gmail.com>
2019-12-10 17:56:26 +0100

    Added output and plot for task d)

--- src/outputtaskd.cpp
+++ src/outputtaskd.cpp
@@ -13,8 +13,9 @@ int main()
     std::array<string, 5> filenr = {"025", "05", "1", "15", "2"};
     string directory = "datafiles/filestaskd/";
     string nr;
-
+    
     Nbody Solver = Nbody();
+    
     for (int i=0; i<filenr.array::size(); i++)
     {   
 
@@ -24,7 +25,18 @@ int main()
         Solver.write_pos(output_filename + nr, directory);
         
     }
-
     
+    std::array<double, 6> beta_lin = {2.0, 2.2, 2.4, 2.6, 2.8, 3.0};
+    std::array<string, 6> filenr_beta = {"2", "22", "24", "26", "28", "30"};
     
+    for (int i=0; i<beta_lin.array::size(); i++)
+    {   
+        nr = filenr_beta[i];
+        Solver = Nbody(1000, 1000, 10000, "datafiles/" + input_filename + filenr[2] + ".txt", false, beta_lin[i]);
+        Solver.velocity_verlet();
+        Solver.write_pos("beta_var" + nr, directory);
+        
+    }    
+    
+    return 0;
 }
--- src/plot_taskd.py
+++ src/plot_taskd.py
@@ -3,9 +3,13 @@ import matplotlib.pyplot as plt
 
 
 file_ending = ["025", "05", "1", "15", "2"]
+beta_ending = ["2", "22", "24", "26", "28", "30"]
+v_init = ["7.088", "8.42", "8.86", "9.746", "13.29"]
+beta = ["2.0", "2.2", "2.4", "2.6", "2.8", "3.0"]
 
 fig, ax = plt.subplots()
 
+c = 0
 for i in file_ending:
     x_coords= np.loadtxt(
         f"datafiles/filestaskd/x_escapevel{i}.txt", skiprows=2
@@ -16,9 +20,31 @@ for i in file_ending:
     z_coords = np.loadtxt(
         f"datafiles/filestaskd/z_escapevel{i}.txt", skiprows=2
     )
-    ax.plot(x_coords[:, 1], y_coords[:, 1])
+    ax.plot(x_coords[:, 1], y_coords[:, 1], label=fr"$v_{{init}}$={v_init[c]}AU/Yr")
+    c += 1
+
+ax.set_xlim([-200, 15])
+ax.set_ylim([-15, 200])
+ax.legend()
+plt.show()
 
 
-ax.set_xlim([-15, 15])
-ax.set_ylim([-15, 15])
+fig, ax = plt.subplots()
+c=0
+for i in beta_ending:
+    x_coords= np.loadtxt(
+        f"datafiles/filestaskd/x_beta_var{i}.txt", skiprows=2
+    )
+    y_coords = np.loadtxt(
+        f"datafiles/filestaskd/y_beta_var{i}.txt", skiprows=2
+    )
+    z_coords = np.loadtxt(
+        f"datafiles/filestaskd/z_beta_var{i}.txt", skiprows=2
+    )
+    ax.plot(x_coords[:, 1], y_coords[:, 1], label=fr"$\beta$={beta[c]}")
+    c += 1
+
+ax.set_xlim([-200, 15])
+ax.set_ylim([-15, 200])
+ax.legend()
 plt.show()
