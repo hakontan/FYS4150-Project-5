9c0dd702c5e596a773e8ce82e973c3e81270b661
Håkon Tansem <hakontan@gmail.com>
2019-12-11 12:42:01 +0100

    Added output directory arguments for outputtaske.cpp

--- src/outputtaske.cpp
+++ src/outputtaske.cpp
@@ -21,21 +21,7 @@ int main(int argc, char* argv[]){
     arma::vec vel = arma::zeros(3);
     double mass = 0;
     string nam = "jeff";
-    //CelestialBody jupiter = CelestialBody(0, 0, 0, 0, 0, 0, 0, nam);
-    //cout << jupiter.mass << endl;
-    //cout << jupiter.name << endl;
-    //jupiter.pos.print();
 
-    //std::vector<CelestialBody> planets = {jupiter};
-    /*
-    SolarSystem test_system = SolarSystem(planets);
-    cout << test_system.bodies[0].name << endl;
-    cout << "1" << endl;
-    SolarSystem jeff = SolarSystem("datafiles/planets_data.txt");
-    cout << "2" << endl;
-    cout << "jeff" << jeff.bodies[0].F << endl;
-    cout << "3" << endl;
-    */
     int Nyr = 15;
     int NperYr = 50000;
     int writenr = 1e4;
@@ -43,17 +29,17 @@ int main(int argc, char* argv[]){
     Nbody Jupiter1 = Nbody(Nyr, NperYr, writenr, "datafiles/data_Sun_Earth_Jupiter.txt", false, 2);
 
     Jupiter1.velocity_verlet();
-    Jupiter1.write_pos("SunEarthJupiter");
+    Jupiter1.write_pos("SunEarthJupiter", "datafiles/outputtaske");
     
     Nbody Jupiter10 = Nbody(Nyr, NperYr, writenr, "datafiles/data_Sun_Earth_Jupiter10.txt", false, 2);
 
     Jupiter10.velocity_verlet();
-    Jupiter10.write_pos("SunEarthJupiter10");
+    Jupiter10.write_pos("SunEarthJupiter10", "datafiles/outputtaske/");
     
     Nbody Jupiter1000 = Nbody(Nyr, NperYr, writenr, "datafiles/data_Sun_Earth_Jupiter1000.txt", false, 2);
 
     Jupiter1000.velocity_verlet();
-    Jupiter1000.write_pos("SunEarthJupiter1000");
+    Jupiter1000.write_pos("SunEarthJupiter1000", "datafiles/outputtaske/");
     
 
     return 0;
