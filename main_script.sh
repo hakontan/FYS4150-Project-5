12f3034bcb20adf4de463ad7fc0ac1eb7209b964
bernharl <bernharl@student.matnat.uio.no>
2019-12-12 09:07:52 +0100

    Added pipenv support to main script

--- main_script.sh
+++ main_script.sh
@@ -14,10 +14,10 @@ echo "Process data? (y/n)"
 read yn
 if [ "$yn" == "y" ]
 then
-  python process_planet_data.py
-  python process_planet_data.py Sun Earth Jupiter
-  python process_planet_data.py Sun Mercury Venus Earth Roadster Mars Jupiter Saturn Neptune Uranus Pluto
-  python process_planet_data.py Sun Mercury
+  pipenv run python process_planet_data.py
+  pipenv run python process_planet_data.py Sun Earth Jupiter
+  pipenv run python process_planet_data.py Sun Mercury Venus Earth Roadster Mars Jupiter Saturn Neptune Uranus Pluto
+  pipenv run process_planet_data.py Sun Mercury
 fi
 
 echo "Generate results task b and c? (y/n)"
@@ -39,9 +39,9 @@ echo "Generate plots for task b and c? (y/n)"
 read yn
 if [ "$yn" == "y" ]
 then
-  python plot_taskb.py
+  pipenv run python plot_taskb.py
   #python plot_sun_earth.py Sun Earth
-  python plot_energies_and_angmom.py
+  pipenv run python plot_energies_and_angmom.py
 fi
 
 echo "Generate results task d? (y/n)"
@@ -56,7 +56,7 @@ echo "Plot results task d? (y/n)"
 read yn
 if [ "$yn" == "y" ]
 then
-  python plot_taskd.py
+  pipenv run python plot_taskd.py
 fi
 
 echo "Generate results task e? (y/n)"
@@ -71,7 +71,7 @@ echo "Plot results task e? (y/n)"
 read yn
 if [ "$yn" == "y" ]
 then
-  python plot_taske.py
+  pipenv run python plot_taske.py
 fi
 
 echo "Generate results task f? (y/n)"
@@ -86,7 +86,7 @@ echo "Plot results task f? (y/n)"
 read yn
 if [ "$yn" == "y" ]
 then
-  python plot_taskf.py
+  pipenv run python plot_taskf.py
 fi
 
 echo "Calculate task g? (y/n)"
